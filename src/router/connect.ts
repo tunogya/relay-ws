84e1fb8e9f286a63d2a22ae7a955d54bff2426d9
tunogya <tunogya@qq.com>
2024-06-19 16:57:34 +0800

    update

--- src/router/connect.ts
+++ src/router/connect.ts
@@ -1,9 +1,12 @@
 import { APIGatewayEvent, Handler } from "aws-lambda";
 
 export const handler: Handler = async (event: APIGatewayEvent, context) => {
+  const connectionId = event.requestContext.connectionId;
   return {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(event),
+    body: JSON.stringify({
+      connectionId: connectionId,
+    }),
   };
 };
