b96f0e3f79265444cbfc939dc554210749dc34ec
tunogya <tunogya@qq.com>
2024-06-19 17:10:09 +0800

    update

--- src/router/disconnect.ts
+++
@@ -1,9 +0,0 @@
-import { APIGatewayEvent, Handler } from "aws-lambda";
-
-export const handler: Handler = async (event: APIGatewayEvent, context) => {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(event),
-  };
-};
