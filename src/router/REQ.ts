c3231fee3f580cbefe8b247be713717993df6c81
tunogya <tunogya@qq.com>
2024-06-20 15:38:22 +0800

    update

--- src/router/REQ.ts
+++ src/router/REQ.ts
@@ -1,6 +1,26 @@
 import { Handler, APIGatewayEvent } from "aws-lambda";
+import {
+  ApiGatewayManagementApiClient,
+  PostToConnectionCommand,
+} from "@aws-sdk/client-apigatewaymanagementapi";
 
 export const handler: Handler = async (event: APIGatewayEvent, context) => {
+  const domain = event.requestContext.domainName;
+  const stage = event.requestContext.stage;
+  const connectionId = event.requestContext.connectionId;
+  const callbackUrl = `https://${domain}/${stage}`;
+  const client = new ApiGatewayManagementApiClient({ endpoint: callbackUrl });
+
+  try {
+    await client.send(
+      new PostToConnectionCommand({
+        ConnectionId: connectionId,
+        Data: "Hello!",
+      }),
+    );
+  } catch (error) {
+    console.log(error);
+  }
   return {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
