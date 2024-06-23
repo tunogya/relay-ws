addc917cf599ecbc01f7ad9938c1041266f38d10
tunogya <tunogya@qq.com>
2024-06-23 15:39:32 +0800

    update

--- src/router/connect.ts
+++ src/router/connect.ts
@@ -6,22 +6,23 @@ export const handler: Handler = async (event: APIGatewayEvent, context) => {
   if (!authorization) {
     return {
       statusCode: 401,
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ error: "Unauthorized" }),
     };
-  } else {
-    authorization = Buffer.from(authorization.split("Basic ")[1]).toString(
-      "base64",
-    );
-    const pubkey = authorization.split(":")[0];
-    await redisClient.set(
-      `pubkey2conn:${pubkey}`,
-      event.requestContext.connectionId,
-      {
-        ex: 24 * 60 * 60,
-      },
-    );
   }
+  authorization = Buffer.from(authorization.split("Basic ")[1]).toString(
+    "base64",
+  );
+  const pubkey = authorization.split(":")[0];
+  const connectionId = event.requestContext.connectionId;
+  await redisClient
+    .pipeline()
+    .incr("ws:connected")
+    .set(`conn2pubkey:${connectionId}`, pubkey, {
+      ex: 24 * 60 * 60,
+    })
+    .set(`pubkey2conn:${pubkey}`, connectionId, {
+      ex: 24 * 60 * 60,
+    })
+    .exec();
   return {
     statusCode: 200,
   };
--- src/router/disconnect.ts
+++ src/router/disconnect.ts
@@ -2,18 +2,19 @@ import { APIGatewayEvent, Handler } from "aws-lambda";
 import redisClient from "../utils/redisClient";
 
 export const handler: Handler = async (event: APIGatewayEvent, context) => {
-  let authorization = event.headers["Authorization"];
-  if (!authorization) {
-    return {
-      statusCode: 401,
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ error: "Unauthorized" }),
-    };
-  } else {
-    authorization = Buffer.from(authorization.split("Basic ")[1]).toString(
-      "base64",
-    );
-    const pubkey = authorization.split(":")[0];
-    await redisClient.del(`pubkey2conn:${pubkey}`);
+  const connectionId = event.requestContext.connectionId;
+  try {
+    const pubkey = await redisClient.get(`conn2pubkey:${connectionId}`);
+    await redisClient
+      .pipeline()
+      .decr("ws:connected")
+      .del(`pubkey2conn:${pubkey}`)
+      .del(`conn2pubkey:${connectionId}`)
+      .exec();
+  } catch (e) {
+    console.log(e);
   }
+  return {
+    statusCode: 200,
+  };
 };
