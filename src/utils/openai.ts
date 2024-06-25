31bcddb92c0469ab311c57444dc33c8589d43500
tunogya <tunogya@qq.com>
2024-06-25 21:24:41 +0800

    update openai gateway

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -1,5 +1,8 @@
 import OpenAI from "openai";
 
-const openai = new OpenAI();
+const openai = new OpenAI({
+  baseURL:
+    "https://gateway.ai.cloudflare.com/v1/702151bcf1ad137360fb347e0353316c/tripiz/openai",
+});
 
 export default openai;
