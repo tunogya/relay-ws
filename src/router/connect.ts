d9820de3bf089fd7d2bd4128c0e29d08a3ab3596
tunogya <tunogya@qq.com>
2024-06-29 02:16:26 +0800

    update

--- src/router/connect.ts
+++ src/router/connect.ts
@@ -3,23 +3,20 @@ import redisClient from "../utils/redisClient";
 
 export const handler: Handler = async (event: APIGatewayEvent, context) => {
   let authorization = event.headers["Authorization"];
-  if (!authorization) {
-    return {
-      statusCode: 401,
-    };
+  if (authorization) {
+    authorization = atob(authorization.replace("Basic ", ""));
+    const pubkey = authorization.split(":")[0];
+    const connectionId = event.requestContext.connectionId;
+    await redisClient
+      .pipeline()
+      .set(`conn2pubkey:${connectionId}`, pubkey, {
+        ex: 24 * 60 * 60,
+      })
+      .set(`pubkey2conn:${pubkey}`, connectionId, {
+        ex: 24 * 60 * 60,
+      })
+      .exec();
   }
-  authorization = atob(authorization.replace("Basic ", ""));
-  const pubkey = authorization.split(":")[0];
-  const connectionId = event.requestContext.connectionId;
-  await redisClient
-    .pipeline()
-    .set(`conn2pubkey:${connectionId}`, pubkey, {
-      ex: 24 * 60 * 60,
-    })
-    .set(`pubkey2conn:${pubkey}`, connectionId, {
-      ex: 24 * 60 * 60,
-    })
-    .exec();
   return {
     statusCode: 200,
   };
